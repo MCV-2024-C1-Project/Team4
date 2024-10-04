8962e65cdb8cde3b2425f6f1c0567c29090ff218
yeray142 <yeraycorderocarrasco@gmail.com>
2024-10-04 11:15:32 +0200

    Adding task 4

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pickle
 import os
 import cv2 as cv
+import argparse
 
 from compute_similarities import compute_similarities
 from compute_descriptors import compute_descriptors
@@ -10,26 +11,32 @@ from utils import plot_hist_task1
 # Get the paths of the folders containing the query set development (QST1), 
 # the paintings dataset (BBDD), and the query set test (QST1)
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-qsd1_path = os.path.join(base_path, "data", "qsd1_w1")
 bbdd_path = os.path.join(base_path, "data", "BBDD")
-qst1_path = os.path.join(base_path, "data", "qst1_w1")
-
-# Load GT (1D histograms computed offline for all images in the BBDD)
-with open(qsd1_path + "/gt_corresps.pkl", 'rb') as f:
-	y = pickle.load(f)
-
 	
 def main():
 
-	# !!!! Read from terminal !!!! Modify according to the method used
-	color_space = "Lab"					# "Lab" or "HSV"
-	k_value = 1	
+	# Read the color space and k value from the command line
+	parser = argparse.ArgumentParser(description="Retrieve results and compute mAP@k")
+	parser.add_argument("color_space", help="Color space (e.g., Lab or HSV)")
+	parser.add_argument("k_value", help="Top k results")
+	parser.add_argument("query_path", help="Path to the query dataset")
+	parser.add_argument("is_test", help="True if we are testing the model (without ground truth)")
+
+	args = parser.parse_args()
+	color_space = args.color_space
+	k_value = int(args.k_value)
+	query_path = args.query_path
+	is_test = args.is_test == "True"
+
+	if not is_test:
+		with open(query_path + "/gt_corresps.pkl", 'rb') as f:
+			y = pickle.load(f)
 
 	# Task 1: Create Museum and query image descriptors (BBDD & QSD1)
 	#	   -> Example of creted image descriptors: For one painting in 
 	# 		  the QST1 we read its '.jpg' file (BGR image). 
 	image_filename = "00024.jpg"
-	image_path = os.path.join(qsd1_path, image_filename)
+	image_path = os.path.join(query_path, image_filename)
 	img = cv.imread(image_path)
 
 	#	   -> Method1: We compute and plot the color histogram of the 
@@ -45,7 +52,7 @@ def main():
 	#	   -> We compute image descriptors for all images in QST1 
 	#		  (are saved in a '.pkl' file) according to the color 
 	# 		  space specified as an argument when running the main()  
-	compute_descriptors(qsd1_path, color_space)
+	compute_descriptors(query_path, color_space)
 
 
 	# Task 2: Implement / compute similarity measures to compare images
@@ -56,8 +63,8 @@ def main():
 	#	   -> We read the '.pkl' files conatining the computed image 
 	# 		  descriptors of the query dataset (QST1) and the museum 
 	# 		  dataset (BBDD, computed offline) 
-	with open(os.path.join(qsd1_path, color_space+'_histograms.pkl'), 'rb') as f:
-		qsd1_histograms = pickle.load(f)
+	with open(os.path.join(query_path, color_space+'_histograms.pkl'), 'rb') as f:
+		query_histograms = pickle.load(f)
 
 	with open(os.path.join(bbdd_path, color_space+'_histograms.pkl'), 'rb') as f:
 		bbdd_histograms = pickle.load(f)
@@ -69,29 +76,27 @@ def main():
 	#			- Method 2 (HSV): Alternative Chi Square (distance)
 	res_m = []
 	if color_space == "Lab":
-
-		for query_lab_img in qsd1_histograms:
+		for query_lab_img in query_histograms:
 			res_m.append(
 				compute_similarities(query_hist=query_lab_img, bbdd_histograms=bbdd_histograms,
 									 method=cv.HISTCMP_HELLINGER, k=k_value)[1])
-
-		print(f"mAP@{k_value} for Method 1 - LAB: {mapk(y, res_m, k_value)}")
 	
 	elif color_space == "HSV":
-
-		for query_hsv_img in qsd1_histograms:
+		for query_hsv_img in query_histograms:
 			res_m.append(
 				compute_similarities(query_hist=query_hsv_img, bbdd_histograms=bbdd_histograms,
 									method=cv.HISTCMP_CHISQR_ALT, k=k_value)[1])
 
-		print(f"mAP@{k_value} for Method 2 - HSV: {mapk(y, res_m, k_value)}")
+	if not is_test:
+		print(f"mAP@{k_value} for {color_space}: {mapk(y, res_m, k_value)}")
 
 	# Task 4: Create predictions for blind challenge (QST1)
-	#	  -> Instead of specifying the QSD1 path, specify the 
-	# 		 QST1 path as argument when running the script
-	#	  -> Jo no tornaria a duplicar tot, faria que el path que ara teniem com a 
-	# 		 QSD1 fos argument que passem des del terminal i així quan executem el 
-	# 		 test no cal tornar a calcular tot lo del QST1
+	if is_test:
+		subdirectory_path = os.path.join(query_path, color_space)
+		os.makedirs(subdirectory_path, exist_ok=True)
+		output_file_path = os.path.join(subdirectory_path, 'result.pkl')
+		with open(output_file_path, 'wb') as f:
+			pickle.dump(res_m, f)
 
 if __name__ == '__main__':
 	main()
