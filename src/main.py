2ebcb9682e599b12beb7763e3cce6a9f4c279efb
Judith Caldés Nevado <125259801+judithcaldes@users.noreply.github.com>
2024-10-05 13:26:20 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ import argparse
 from compute_similarities import compute_similarities
 from compute_descriptors import compute_descriptors
 from average_precision import mapk
-from utils import plot_hist_task1
 
 # Get the path of the folder containing the museum dataset (BBDD)
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,7 +13,7 @@ bbdd_path = os.path.join(base_path, "data", "BBDD")
 	
 def main():
 
-	# Read the color space, k value, query dataset path, and test flag from the command line
+	# Read the color space, similarity measure, k value, query dataset path, and test flag from the command line
 	parser = argparse.ArgumentParser(description="Retrieve results and compute mAP@k")
 	parser.add_argument("color_space", help="Color space (e.g., Lab, HSV)")
 	parser.add_argument("similarity_measure", help="Similarity Measure (e.g., HISTCMP_HELLINGER, HISTCMP_CHISQR_ALT)")
@@ -47,49 +46,42 @@ def main():
 	else:
 		raise ValueError(f"Unknown similarity measure: {similarity_measure}")
 
+	## Task 3: Implement retrieval system (retrieve top K results)
+
 	# If we are not testing, we get the provided GT to evaluate the results 
 	# obatined for the QSD1
 	if not is_test:
 		with open(q_path + "/gt_corresps.pkl", 'rb') as f:
-			y = pickle.load(f)
-	
-	#	   -> We compute image descriptors for all images in QST1 
-	#		  (are saved in a '.pkl' file) according to the color 
-	# 		  space specified as an argument when running the main()  
+			y = pickle.load(f) 
+
+	# Compute image descriptors for all images in the query dataset (QST1) 
+	# and save them in a '.pkl' file based on the specified color space
 	compute_descriptors(q_path, color_space)
-
-
-	# Task 2: Implement / compute similarity measures to compare images
-	#	   -> Selection of similarity measures is discussed in the README.md file	
-
-
-	# Task 3: Implement retrieval system (retrieve top K results)
-	#	   -> We read the '.pkl' files conatining the computed image 
-	# 		  descriptors of the query dataset (QST1) and the museum 
-	# 		  dataset (BBDD, computed offline) 
+	
+	# Load the precomputed image descriptors from '.pkl' files
+	# for both the query dataset (QST1) and the museum dataset (BBDD, computed offline)
 	with open(os.path.join(q_path, color_space+'_histograms.pkl'), 'rb') as f:
 		query_histograms = pickle.load(f)
 
 	with open(os.path.join(bbdd_path, color_space+'_histograms.pkl'), 'rb') as f:
 		bbdd_histograms = pickle.load(f)
 
-	#	   -> For each image in the query set (QST1), we compute its 
-	# 		  similarity to all museum images (BBDD). The best results   
-	# 		  are obtained using the following similarity measures:
-	#			- Method 1 (Lab): Hellinger (distance)
-	#			- Method 2 (HSV): Alternative Chi Square (distance)
+	# For each image in the query set (QST1), compute its similarity to all museum images (BBDD).
+	# The best results are obtained using the following similarity measures:	  
+	#	- Method 1 (Lab): Hellinger (distance)
+	#	- Method 2 (HSV): Alternative Chi Square (distance)
 	res_m = []
 	
 	for query_img_h in query_histograms:
 		res_m.append(compute_similarities(query_img_h, bbdd_histograms, similarity_function, k_value)[1])
 
-	#print(res_m)
-
+	# Evaluate the results using mAP@K if we are not in testing mode
 	if not is_test:
 		print(f"mAP@{k_value} for {color_space}: {mapk(y, res_m, k_value)}")
 
-	# Task 4: Create predictions for blind challenge (QST1)
-	#	   -> Save the 'blind' results for the test query set 
+	## Task 4: Create predictions for blind challenge (QST1)
+
+	# Save the 'blind' results for the test query set 
 	if is_test:
 		subdirectory_path = os.path.join(q_path, color_space)
 		os.makedirs(subdirectory_path, exist_ok=True)
