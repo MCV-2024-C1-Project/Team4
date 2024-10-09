8420024b2ba105c1aa0ec04a05f3a6cec97b9da1
yeray142 <yeraycorderocarrasco@gmail.com>
2024-10-09 10:58:46 +0200

    Add background removal

---
+++ week2/src/background_removal.py
@@ -0,0 +1,79 @@
+import cv2 as cv
+import argparse
+import numpy as np
+
+
+def fill_surrounded_pixels(foreground):
+	h, w = foreground.shape
+	new_mask = foreground.copy()
+
+	for i in range(h):
+		for j in range(w):
+			if foreground[i, j] == 0:
+				# Check if there is a 1 above
+				has_one_above = np.any(foreground[:i, j] == 1) if i > 0 else False
+				# Check if there is a 1 below
+				has_one_below = np.any(foreground[i + 1:, j] == 1) if i < h - 1 else False
+				# Check if there is a 1 to the left
+				has_one_left = np.any(foreground[i, :j] == 1) if j > 0 else False
+				# Check if there is a 1 to the right
+				has_one_right = np.any(foreground[i, j + 1:] == 1) if j < w - 1 else False
+
+				# If there is at least a 1 in each direction, change the pixel to 1
+				if has_one_above and has_one_below and has_one_left and has_one_right:
+					new_mask[i, j] = 1
+	return new_mask
+
+
+
+def main():
+	# Get the image path argument
+	parser = argparse.ArgumentParser(description="Remove background from an image")
+	parser.add_argument("image_path", help="Path to the image")
+	args = parser.parse_args()
+
+	# Read image
+	image = cv.imread(args.image_path)
+
+	# Show the original image
+	cv.imshow("Original Image", image)
+	cv.waitKey(0)
+
+	myimage_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+	# Take S and remove any value that is less than half
+	s = myimage_hsv[:, :, 1]
+	s = np.where(s < 127, 0, 1)  # Any value below 127 will be excluded
+
+	# We increase the brightness of the image and then mod by 255
+	v = (myimage_hsv[:, :, 2] + 127) % 255
+	v = np.where(v > 255, 1, 0)  # Any value above 127 will be part of our mask
+
+	# Combine our two masks based on S and V into a single "Foreground"
+	foreground = np.where(s + v > 0, 1, 0).astype(np.uint8)
+	foreground[:10, :] = 0; foreground[-10:, :] = 0; foreground[:, :10] = 0; foreground[:, -10:] = 0
+	foreground = fill_surrounded_pixels(foreground)
+	print(foreground)
+
+	# Show foreground mask
+	cv.imshow("Foreground Mask", foreground * 255)
+	cv.waitKey(0)
+
+	background = np.where(foreground == 0, 255, 0).astype(np.uint8)  # Invert foreground to get background in uint8
+	print(background)
+
+	# Show background mask
+	background = cv.cvtColor(background, cv.COLOR_GRAY2BGR)  # Convert background back into BGR space
+	cv.imshow("Background Mask", background)
+	cv.waitKey(0)
+
+	foreground = cv.bitwise_and(image, image, mask=foreground)  # Apply our foreground map to original image
+
+	# Show the final image
+	finalimage = foreground - background  # Combine foreground and background
+	cv.imshow("Final Image", finalimage)
+	cv.waitKey(0)
+
+
+if __name__ == "__main__":
+	main()
