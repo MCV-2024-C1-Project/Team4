98cd47e0f36c4b9866d964908c951de91422cf21
carmecorbi <carme.corbi.sule@gmail.com>
2024-10-20 10:01:34 +0200

    Update compute_db_descriptors.py

--- week3/src/compute_db_descriptors.py
+++ week3/src/compute_db_descriptors.py
@@ -17,19 +17,14 @@ base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fi
 def main():
 
 	# Argument parser
-	parser = argparse.ArgumentParser(description="Compute 3D histograms for each image in the BBDD")
-	parser.add_argument('--color_space', type=str, choices=['Lab', 'HSV', 'RGB', 'HLS', 'Luv', 'YCrCb', 'YUV'], help='Color space to use')
+	parser = argparse.ArgumentParser(description="")
 	parser.add_argument('--num_blocks', type=int, help='Number of blocks for block histogram')
-	parser.add_argument('--num_levels', type=int, help='Number of levels for block histogram')
-	parser.add_argument('--num_bins', type=int, help='Number of bins for histogram')
-	parser.add_argument("--is_pyramid", help="True if we are using the spatial pyramid histogram mode", default=False, type=bool)
+	parser.add_argument('--descriptor_type')
 	args = parser.parse_args()
 
-	COLOR_SPACE = args.color_space
 	NUM_BLOCKS = args.num_blocks
-	NUM_LEVELS = args.num_levels
-	NUM_BINS = args.num_bins
-	is_pyramid = args.is_pyramid
+	DESCRIPTOR_TYPE = args.descriptor_type
+	print(DESCRIPTOR_TYPE)
 
 	# Compute descriptors for the BBDD images (offline)
 	imgs_path = os.path.join(base_path, "data", "BBDD")
@@ -42,37 +37,10 @@ def main():
 		# Read image (by default the color space of the loaded image is BGR) 
 		img_bgr = cv.imread(os.path.join(imgs_path, filename))
 
-		# Change color space (only 2 options are possible)
-		if COLOR_SPACE == "Lab":
-			img = cv.cvtColor(img_bgr, cv.COLOR_BGR2Lab)
-			ranges = [0,256,0,256,0,256]
-		elif COLOR_SPACE == "HSV":
-			img = cv.cvtColor(img_bgr, cv.COLOR_BGR2HSV)
-			ranges = [0,180,0,256,0,256]
-		elif COLOR_SPACE == "RGB":
-			img = cv.cvtColor(img_bgr, cv.COLOR_BGR2RGB)
-			ranges = [0,256,0,256,0,256]
-		elif COLOR_SPACE == "HLS":
-			img = cv.cvtColor(img_bgr, cv.COLOR_BGR2HLS)
-			ranges = [0,256,0,256,0,256]
-		elif COLOR_SPACE == "Luv":
-			img = cv.cvtColor(img_bgr, cv.COLOR_BGR2Luv)
-			ranges = [0,256,0,256,0,256]
-		elif COLOR_SPACE == "YCrCb":
-			img = cv.cvtColor(img_bgr, cv.COLOR_BGR2YCrCb)
-			ranges = [0,256,0,256,0,256]
-		elif COLOR_SPACE == "YUV":
-			img = cv.cvtColor(img_bgr, cv.COLOR_BGR2YUV)
-			ranges = [0,256,0,256,0,256]
-
-		# Compute the 3D histogram
-
-		if not is_pyramid:
-			# Compute the 3D Block Histograms for the query image
-			hist = block_histogram(img,NUM_BLOCKS,NUM_BINS,ranges)
-		else:
-			# Compute the 3D Hierarchical Histograms for the query image
-			hist = spatial_pyramid_histogram(img, NUM_LEVELS, NUM_BINS, ranges)
+		if DESCRIPTOR_TYPE == 'LBP':
+			img = cv.imread(os.path.join(imgs_path, filename),cv.COLOR_BGR2GRAY)
+			hist = lbp_block_histogram(img,(NUM_BLOCKS,NUM_BLOCKS))
+			
 
 		index = int(filename.split('_')[-1].split('.')[0])
 
@@ -80,12 +48,10 @@ def main():
 			histograms.extend([None] * (index + 1 - len(histograms)))
 		histograms[index] = hist
 
-	if not is_pyramid:
-		with open(os.path.join(imgs_path, COLOR_SPACE + '_histograms_'+str(NUM_BLOCKS)+'_blocks_'+str(NUM_BINS)+'_bins'+'.pkl'), 'wb') as f:
-			pickle.dump(histograms, f)
-	else:
-		with open(os.path.join(imgs_path, COLOR_SPACE + '_histograms_'+str(NUM_LEVELS)+'_levels_'+str(NUM_BINS)+'_bins'+'.pkl'), 'wb') as f:
-			pickle.dump(histograms, f)
+	
+	with open(os.path.join(imgs_path, f'{DESCRIPTOR_TYPE}_histograms_{NUM_BLOCKS},{NUM_BLOCKS}_blocks_.pkl'), 'wb') as f:
+		pickle.dump(histograms, f)
+	
 
 
 if __name__ == "__main__":
