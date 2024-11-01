ca5ae4d653ba45f4fbd675cbf9cb6515e69dbb78
carmecorbi <carme.corbi.sule@gmail.com>
2024-11-01 20:49:18 +0100

    Update compute_similarities.py

--- week4/src/compute_similarities.py
+++ week4/src/compute_similarities.py
@@ -19,23 +19,28 @@ def compute_similarities(query_descriptors: Any, bbdd_descriptors: Any, des_type
     results = []
     
     for idx, bbdd_desc in enumerate(bbdd_descriptors):
-        # Use the match function to find the number of matches between query and BBDD descriptors
-           if bbdd_desc is None:
+        if bbdd_desc is None:
             num_good_matches = 0  # Si bbdd_desc es None, asignar 0
-           else:
-        # Usar la función match para encontrar el número de coincidencias
+            results.append((idx, num_good_matches))
+        else:
             matches = match(query_descriptors, bbdd_desc, des_type)
             num_good_matches = len(matches)  # Contar las coincidencias
+            results.append((idx, num_good_matches))
+
+        
 
     # Agregar el resultado a la lista
-    results.append((idx, num_good_matches))
+    
        
-        
+    print(results) 
     # Sort results by the number of good matches in descending order (more matches is better)
     results.sort(key=lambda x: x[1], reverse=True)
+    print(results)
 
     # Get the indices of the results
     results_idx = [result[0] for result in results]
+
+    
 
     # Return the k best matches
     return results[:k], results_idx[:k]
